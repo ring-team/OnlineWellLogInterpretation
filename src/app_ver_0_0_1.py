fccc387f35220b56dd7526f49592bd63e903b6f0
MateoBontempsRodrigues <mateo.btmpsrodrigues@gmail.com>
2025-10-09 11:42:04 +0200

    background image forced and form download button at the correct spot, test well log image

--- src/app_ver_0_0_1.py
+++ src/app_ver_0_0_1.py
@@ -1,5 +1,5 @@
-import streamlit as st
-from streamlit_drawable_canvas import st_canvas
+import streamlit as st #1.28.0
+from streamlit_drawable_canvas import st_canvas #0.9.3
 from PIL import Image
 import pandas as pd
 import json 
@@ -11,6 +11,10 @@ import json
 #   - Affichage des données à interpreter
 #   - Entrée de l'interprétation -> Quel format ?
 #   - Récupération de l'interpretation ? -> Comment ?
+
+
+st.set_page_config(layout="wide")  # Utilise toute la largeur de l'écran
+
 
 #Presentation de l'experience
 
@@ -42,6 +46,20 @@ confidence = st.radio(
     ["I am the best there is", "I'm excellent", "I'm quite good", "Neutral", "not particularly confident", "Not confident", "First time in my life I see well log data", "Geosciences ? What's that ?"]
 )
 
+#Enregistrement des réponses dans un fichier JSON
+
+data = {
+    "Status": status,
+    "field": field,
+    "other_field": other_field,
+    "years": years,
+    "confidence": confidence
+
+}
+
+if st.button("Download your answers in a JSON file !"):
+    with open(r"C:\Users\e3812u\Documents\Projet_3A\OnlineWellLogInterpretation\Results\test.json", "w") as f:
+        json.dump(data, f)
 
 #Introduction à l'expérience d'interpretation
 st.title("The interpretation experiment")
@@ -58,7 +76,7 @@ st.write("The data you are to interprete is/is from a public dataset available o
 st.title("Interpretation")
 
 # Specify canvas parameters in application
-col1, col2, col3 = st.columns([1, 1, 1], vertical_alignment="top")
+col1, col2, col3 = st.columns([1, 1, 1])
 drawing_mode = col1.selectbox(
     "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
 )
@@ -66,9 +84,9 @@ drawing_mode = col1.selectbox(
 stroke_width = col2.slider("Stroke width: ", 1, 25, 3)
 if drawing_mode == 'point':
     point_display_radius = col2.slider("Point display radius: ", 1, 25, 3)
-stroke_color = col3.color_picker("Stroke color hex: ")
-bg_color = col3.color_picker("Background color hex: ", "#eee")
-bg_image = col1.file_uploader("Background image:", type=["png", "jpg"]) # Will be removed and the background image is going to be forced to be the one from the data.
+stroke_color = col1.color_picker("Stroke color hex: ")
+#bg_color = col3.color_picker("Background color hex: ", "#eee")
+bg_image =  r"Data\test_well_log.png" #Forced background = well log.
 
 realtime_update = col3.checkbox("Update in realtime", True)
 
@@ -79,10 +97,11 @@ canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     stroke_color=stroke_color,
-    background_color=bg_color,
+    #background_color=bg_color,
     background_image=Image.open(bg_image) if bg_image else None,
     update_streamlit=realtime_update,
     height=750,
+    width=800,
     drawing_mode=drawing_mode,
     point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
     key="canvas",
@@ -91,17 +110,4 @@ canvas_result = st_canvas(
 
 
 
-#Enregistrement des réponses dans un fichier JSON
 
-data = {
-    "Status": status,
-    "field": field,
-    "other_field": other_field,
-    "years": years,
-    "confidence": confidence
-
-}
-
-if st.button("Download"):
-    with open(r"C:\Users\e3812u\Documents\Projet_3A\OnlineWellLogInterpretation\Results\test.json", "w") as f:
-        json.dump(data, f)
